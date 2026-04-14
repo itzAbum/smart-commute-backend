a0b9c92987298601918efa817ba78cbfc55c7ab9
Aahmed <155355389+itzAbum@users.noreply.github.com>
2026-04-13 22:38:10 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,32 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Time
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.dialects.mysql import JSON
 from sqlalchemy.orm import relationship
 
 from database import Base
-
-
-
-class UsersNew(Base):
-    __tablename__ = "users_new"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(255), unique=True, index=True, nullable=False)
-    password_hash = Column(String(255), nullable=False)
-
-    profile = relationship("UserProfilesNew", back_populates="user", uselist=False)
-    schedules = relationship("SchedulesNew", back_populates="user")
-
-
-class UserProfilesNew(Base):
-    __tablename__ = "user_profiles_new"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users_new.id"), nullable=False)
-    email = Column(String(255), nullable=True)
-    home_address = Column(String(255), nullable=True)
-
-    user = relationship("UsersNew", back_populates="profile")
-
 
 class SchedulesNew(Base):
     __tablename__ = "schedules_new"
@@ -35,7 +11,7 @@ class SchedulesNew(Base):
     user_id = Column(Integer, ForeignKey("users_new.id"), nullable=False)
     course = Column(String(255), nullable=False)
     days = Column(JSON, nullable=False)  # ["Monday", "Wednesday", ...]
-    start = Column(Time, nullable=False)
-    end = Column(Time, nullable=False)
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
 
     user = relationship("UsersNew", back_populates="schedules")
